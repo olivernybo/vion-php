5079ed0acda137ec7311343d5b071f9dd1fced83
Voss19 <silvermaster13@gmail.com>
2018-12-05 13:38:17 +0100

    Initial commit

---
+++ application/config/vion.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// User database table
+$config['user_db_table'] = 'users';
+// What key to search in the database
+$config['user_db_search_key'] = 'u_email';
+
+// What to set the user data key as
+$config['data_user_key'] = 'user';
+// What key to search in the sessions
+$config['session_user_key'] = 'user';
+
+// The template folder the loader should look for
+$config['template_folder'] = 'templates';
+
+$config['templates'] = array(
+	'main' => array(
+		'top' => array(
+			'main/head',
+			'main/nav'
+		),
+		'bottom' => array(
+			'main/footer',
+			'main/end'
+		)
+	)
+);
+
+$config['standard_template'] = 'main';
+
+$config['constants'] = array(
+	'css' => array(
+		array(
+			'style' => base_url('css1')
+		),
+		array(
+			'style' => base_url('css2')
+		)
+	)
+);
---
+++ application/libraries/Vion.php
@@ -0,0 +1,116 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Vion {
+
+	private $ci;
+
+	public $views;
+	public $data;
+	public $user;
+
+	public function __construct()
+	{
+		$this->ci = &get_instance();
+
+		$this->ci->config->load('vion');
+		$this->ci->load->database();
+		$this->ci->load->library('session');
+		$this->ci->load->library('parser');
+		
+		$this->data = array();
+		$this->views = array();
+
+		foreach ($this->ci->config->item('constants') as $key => $value) {
+			$this->set_data($value, $key);
+		}
+	}
+	
+	public function view($template = null)
+	{
+		$this->add_view($this->ci->router->fetch_method(), $this->ci->router->fetch_class().'/')
+			->add_template($template)
+			->load_user_data()
+			->parse_views();
+	}
+
+	public function add_view($html, $folder = '')
+	{
+		if (is_array($html)) {
+			foreach ($html as $value) {
+				$this->views[] = $folder.$value;
+			}
+		} else if (is_string($html)) {
+			$this->views[] = $folder.$html;
+		} else {
+			throw new Exception('First argument must be type string or array.');
+		}
+
+		return $this;
+	}
+
+	public function parse_view($html)
+	{
+		$this->ci->parser->parse($html, $this->data);
+	}
+
+	public function parse_views()
+	{
+		foreach ($this->views as $html) {
+			$this->parse_view($html);
+		}
+	}
+
+	public function load_user_data()
+	{
+		$sess_user_key = $this->ci->config->item('session_user_key') ?: 'user';
+
+		if ($this->ci->session->$sess_user_key) {
+			$query = $this->ci->db->where($this->ci->config->item('user_db_search_key'), $this->ci->session->user[$this->ci->config->item('user_db_search_key')])->get($this->ci->config->item('user_db_table'));
+
+			if ($query->num_rows()) {
+				$this->user = $query->result_array();
+				$user_key = $this->ci->config->item('data_user_key') ?: 'user';
+				$this->set_data($user_key, $this->user);
+			}
+		}
+
+		return $this;
+	}
+
+	public function add_template($template = null)
+	{
+		$template_top = array();
+		$template_bottom = array();
+
+		$templates = $this->ci->config->item('templates');
+		$t = $template ?: $this->ci->config->item('standard_template');
+		$temp = $templates[$t];
+		$folder = $this->ci->config->item('template_folder').'/';
+
+		foreach ($temp['top'] as $html) {
+			$template_top[] = $folder.$html;
+		}
+
+		foreach ($temp['bottom'] as $html) {
+			$template_bottom[] = $folder.$html;
+		}
+
+		$this->views = array_merge($template_top, $this->views, $template_bottom);
+
+		return $this;
+	}
+
+	public function set_data($data, string ...$args)
+	{
+		$curr = &$this->data;
+
+		foreach($args as $key) {
+		   $curr = &$curr[$key];
+		}
+
+		$curr = $data;
+
+		return $this;
+	}
+}
